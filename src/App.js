73c741b4e91c4067dadee3f5044d804c38badbb5
Koi <Makoi.Gacula@student.csulb.edu>
2020-11-11 23:29:19 -0800

    added Unspash URLs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+// const clientID = `?client_id=${process.env.dfr0Pedr_oDp7FUuaDFuBCx4Z5VWWUXU2FuHZTxGg3E}`
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
-function App() {}
+function App() {
+  return <h2>stock photos starter</h2>;
+}
 
 export default App;
