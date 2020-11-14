5870384b965731952498b13bd4bde5db03552fac
Koi <Makoi.Gacula@student.csulb.edu>
2020-11-13 18:49:22 -0800

    Completed infinite scroll to fetch photos from API once reaching the bottom of the page

--- src/App.js
+++ src/App.js
@@ -16,19 +16,29 @@ function App() {
   // set up state hooks
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
 
   // async function to fetch data from API
   const fetchImages = async () => {
     setLoading(true);
-    let url; // uses let b/c url will change between searching & default images
-    url = `${mainUrl}${clientID}`;
+    let url; // initialize w/ let b/c url will change between default images & searched images
+    // Uses state to access page number of url
+    // page: the page number to retrieve  (a parameter)
+    const urlPage = `&page=${page}`;
+
+    url = `${mainUrl}${clientID}${urlPage}`;
     console.log(url);
+
     try {
-      const response = await fetch(url);
+      const response = await fetch(url); // invokes a fetch request to retrieve photos from url
       const data = await response.json();
       console.log(data);
 
-      setPhotos(data);
+      // Updates images by copying over old state values, every time we scroll to a new page
+      setPhotos((oldImages) => {
+        return [...oldImages, ...data];
+      });
+      // sets to not loading
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -36,8 +46,30 @@ function App() {
     }
   };
 
+  // useEffect that fetching images from API.
   useEffect(() => {
     fetchImages();
+  }, [page]); // re-renders the fetchImages component every time the page changes
+
+  // useEffect that listens for a scroll event
+  useEffect(() => {
+    const scrollEvent = window.addEventListener("scroll", () => {
+      // console.log(`innerHeight: ${window.innerHeight}px`);
+      // console.log(`scrollY: ${window.scrollY}px`);
+      // console.log(`body height: ${document.body.scrollHeight}px`);
+
+      // If we reach the end of the document minus 2px, then we fetch more photos
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        // console.log("reached the end of the document");
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+    return () => window.removeEventListener("scroll", scrollEvent);
   }, []);
 
   const handleSubmit = (e) => {
@@ -58,6 +90,7 @@ function App() {
       <section className="photos">
         <div className="photos-container">
           {photos.map((image, index) => {
+            // console.log(image);
             // uses spread operator to pass all props of the image property
             return <Photo key={index} {...image} u />;
           })}
