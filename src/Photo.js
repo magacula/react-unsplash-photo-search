be12ae4c75462cacc9b359297561fbfa9e17d838
Koi <Makoi.Gacula@student.csulb.edu>
2020-11-14 16:33:22 -0800

    updated profile image style

--- src/Photo.js
+++ src/Photo.js
@@ -21,7 +21,7 @@ const Photo = ({
           <p>{likes} likes</p>
         </div>
         <a href={portfolio_url}>
-          <img src={medium} alt={name} />
+          <img src={medium} alt={name} className="user-img" />
         </a>
       </div>
     </article>
